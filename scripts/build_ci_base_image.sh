ae152a23a688694d9b8f9063237b24840863348b
Marcin Sobczyk <marcinpiotrsobczyk@gmail.com>
2023-11-01 11:51:57 +0100

    rename boilerplate_image

--- scripts/build_ci_base_image.sh
+++ scripts/build_ci_base_image.sh
@@ -6,6 +6,6 @@ set -xeuo pipefail
 COMMIT=${1:-"Missing commit sha"}
 
 docker build --progress=plain --target=cxx_boilerplate_image \
-  -t "boilerplate_image:$COMMIT" -f Dockerfile .
+  -t "cxx_boilerplate_image:$COMMIT" -f Dockerfile .
 
-docker tag "boilerplate_image:$COMMIT" cxx_boilerplate_image
+docker tag "cxx_boilerplate_image:$COMMIT" cxx_boilerplate_image
