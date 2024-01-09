2d7841d21da4537941c9267f53375e03fe8a4a1e
Marcin Sobczyk <marcinpiotrsobczyk@gmail.com>
2024-01-09 18:02:54 +0100

    example parse json

---
+++ programs/parse/parse.cpp
@@ -0,0 +1,32 @@
+
+#include <cstring>
+#include <exception>
+#include <filesystem>
+#include <iostream>
+
+#include "glaze/json/read.hpp"
+#include "glaze/json/write.hpp"
+
+int main(int argc, const char **argv) {
+  const char *filename;
+
+  if (argc>=2) {
+    filename = argv[1];
+  } else {
+    filename = "example.json";
+  }
+  std::cout << filename << "\n";
+
+  glz::json_t json{};
+  auto ec = glz::read_file_json(json, filename, std::string{});
+  if (ec) {
+    throw std::runtime_error("ec");
+  }
+
+  std::string out;
+  glz::write_json(json, out);
+
+  std::cout << out << "\n";
+
+  return 0;
+}
